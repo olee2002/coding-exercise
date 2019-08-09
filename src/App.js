2dfc09a36e4adfae42ef89ece4b03217ce954086
babylee2002 <babylee2002@gmail.com>
2019-08-09 10:54:43 -0400

    header styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
 import Card from './components/Card'
+import Header from './components/Header'
 
 import data from './data/UIE-InterviewProject.json'
 import './styles/style.css';
 
 function App() {
   return (
+     <>
+   <Header/>
     <div className="container">
       {data.map((info, i)=><Card key={i} data = {info}/>)}
     </div>
+    </>
   );
 }
 
