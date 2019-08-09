8182fb9eecffd9c10e228c2cf6510d91b1488265
babylee2002 <babylee2002@gmail.com>
2019-08-09 11:08:30 -0400

    header added for sorting

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Header() {
    return (
       <div className='header'>
-         Hello! from header!
+            <h6>Sort By: </h6>
+            <button className='btn btn-primary m-1'>Heading</button>
+            <button className='btn btn-primary m-1'>Sub Heading</button>
+            <button className='btn btn-primary m-1'>Price</button>
       </div>
    )
 }
