514b0f6bcc6a1a7b67540643d5b9e646bc9f1b6a
babylee2002 <babylee2002@gmail.com>
2019-08-09 08:52:56 -0400

    bridge image showing

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
-export default function Card() {
+export default function Card({ data }) {
+   console.log('cardData', data)
    return (
       <div className='card'>
-         test
+         {data && data.showBridge ? 
+         <img src='http://res.cloudinary.com/bguggie/image/upload/v1425514888/wide_ggbridge_bg_teneax.jpg'/> 
+         :  
+         <div className='image'></div>}
+        
+         <div className='card-content'>
+
+         </div>
       </div>
    )
 }
