8db8ecd13434fcb90eafe285a1d846532351d271
babylee2002 <babylee2002@gmail.com>
2019-08-09 07:54:40 -0400

    card component brought into App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 
+import Card from './components/Card'
+
 import data from './data/UIE-InterviewProject.json'
 
 import './styles/style.css';
-console.log('data', data)
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.map((info, i)=><Card key={i} data = {info}/>)}
     </div>
   );
 }
