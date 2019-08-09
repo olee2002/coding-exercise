4d58c9b9475cb4f6a9342d9252eb1cadbacb4aec
babylee2002 <babylee2002@gmail.com>
2019-08-09 09:49:24 -0400

    uppercase function added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 export default function Card({ data }) {
-   console.log('cardData', data)
+   const upperCase = string=>{
+      const strArr = string.split(' ')
+      const strArrCopy = strArr.map(str=>str.charAt(0).toUpperCase() + str.slice(1))
+      return strArrCopy.join(' ')
+   }
    return (
       <div className='card'>
          {data && data.showBridge ? 
@@ -15,10 +19,13 @@ export default function Card({ data }) {
          src='http://res.cloudinary.com/bguggie/image/upload/v1425514736/place_holder_zuvywg.png'
          alt='placeholder'
          /> 
-         </div>}
-        
+         </div>} 
          <div className='card-content'>
-
+            <div className='sub-content'>
+            <h5 className='heading'>{upperCase(data.Heading)}</h5>
+            <p className='subheading'>{upperCase(data.Subheading)}</p>
+            </div>
+            <h5 className='price'>{data.Price}</h5>
          </div>
       </div>
    )
